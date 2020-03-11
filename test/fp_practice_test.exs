f6172c2655927a53d2b690a6169869516c656936
PastLeo <chgu82837@gmail.com>
2020-03-11 17:49:09 +0800

    tolerate different order of full_names in test

--- test/fp_practice_test.exs
+++ test/fp_practice_test.exs
@@ -2,24 +2,47 @@ defmodule FpPracticeTest do
   use ExUnit.Case
   doctest FpPractice
 
+  defp sort_names(names_group) do
+    names_group
+    |> map_values(fn first_names ->
+      map_values(first_names, &Enum.sort/1)
+    end)
+  end
+
+  defp map_values(map, func) when is_map(map) do
+    map
+    |> Enum.map(fn {k, v} -> {k, func.(v)} end)
+    |> Enum.into(%{})
+  end
+  defp map_values(any, _), do: any
+
   test "convert classes to group of first names and staff/student" do
-    assert FpPractice.student_staff_name_group(FpPractice.test_classes()) == %{
-             staff: %{
-               "Gale" => ["Gale Christensen"],
-               "Liliana" => ["Liliana Kidd"],
-               "Liza" => ["Liza Vega"],
-               "Luz" => ["Luz Crawford"],
-               "Ollie" => ["Ollie Matthews"],
-               "Rosemary" => ["Rosemary Casey"],
-               "Tricia" => ["Tricia Brewer"]
-             },
-             students: %{
-               "Gale" => ["Gale Strong", "Gale Joseph", "Gale Bowers"],
-               "Luz" => ["Luz Casey"],
-               "Ollie" => ["Ollie Sandoval"],
-               "Ophelia" => ["Ophelia Sears", "Ophelia Crawford"],
-               "Rosemary" => ["Rosemary Barber"]
-             }
-           }
+    result =
+      FpPractice.test_classes()
+      |> FpPractice.student_staff_name_group()
+      |> sort_names()
+
+    expected = sort_names(
+      %{
+        staff: %{
+          "Gale" => ["Gale Christensen"],
+          "Liliana" => ["Liliana Kidd"],
+          "Liza" => ["Liza Vega"],
+          "Luz" => ["Luz Crawford"],
+          "Ollie" => ["Ollie Matthews"],
+          "Rosemary" => ["Rosemary Casey"],
+          "Tricia" => ["Tricia Brewer"]
+        },
+        students: %{
+          "Gale" => ["Gale Strong", "Gale Joseph", "Gale Bowers"],
+          "Luz" => ["Luz Casey"],
+          "Ollie" => ["Ollie Sandoval"],
+          "Ophelia" => ["Ophelia Crawford", "Ophelia Sears"],
+          "Rosemary" => ["Rosemary Barber"]
+        }
+      }
+    )
+
+    assert result == expected
   end
 end
